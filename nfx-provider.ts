4ef9b7e4ded66e385a6052f1a6c4161863cc9bcb
Narutinho Cerejão <iotlinkchainproject@gmail.com>
2026-06-08 08:00:38 -0300

    improvments structure

--- nfx-provider.ts
+++
@@ -1,6 +0,0 @@
-// NFX Provider Integration
-import { NFXProvider } from '../../nfx-dapp-provider/src/nfx-provider.js';
-
-if (typeof window !== 'undefined') {
-    window.nfx = new NFXProvider();
-}
